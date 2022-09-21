87a65a94e8c84f2e6143d00c595376ce8ac318c1
Igor Montagner <igordsm@gmail.com>
2022-09-21 16:55:05 -0500

    Working first implementation of Parsons problems.

--- src/active_handout_plugins/parsons.py
+++ src/active_handout_plugins/parsons.py
@@ -1,38 +1,44 @@
 import xml.etree.ElementTree as etree
 from .utils import AdmonitionVisitor
 from .question import QuestionAdmonition
+import random
 
 class ParsonsQuestion(QuestionAdmonition):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__('question', ['parsons'], *args, **kwargs)
-        self.counter = 0
 
     def create_question_form(self, el, submission_form):
         code = submission_form.findall('*')[-2]
         end_char = code.text.find("\x03")
         start_index = code.text.find(":") + 1
-        breakpoint()
         html_idx = int(code.text[start_index:end_char])
         processed_code = self.md.htmlStash.rawHtmlBlocks[html_idx]
         
         start_index = processed_code.find("<code>") + 6
-
         lines = processed_code.split("\n")[:-1]
         lines[0] = lines[0][start_index:]
 
-        print(lines)
-        etreeCode = etree.fromstring(processed_code)
+        random.shuffle(lines)
+        left_panel = '<div class="highlight original-code"><pre><code class="parsons-drag-area">'
+        for l in lines:
+            indent_count = l.count('    ')
+            l_no_indent = l.replace('    ', '')
+            left_panel += f'<div class="parsons-line" draggable="true" data-indentCount={indent_count}>{l_no_indent}</div>'
+        left_panel += '</code></pre></div>'
         
-        left_panel = etree.tostring(etreeCode, encoding='unicode', method='html')
         right_panel = '''
-<div class="highlight"><pre><code><p>a</p></code></pre></div>
+<div class="highlight parsons-drop-div">
+    <pre><code class="parsons-drop-area"><div class="indent-line"> </div></code></pre>
+</div>
 '''
-        code.set("data-pnum", str(self.counter))
         code.set("class", "parsons-code")
+        code.tag = 'div'
         code.text = self.md.htmlStash.store(left_panel + right_panel)
-        breakpoint()
 
-        self.counter += 1
-        return '<input type="submit" name="sendButton" value="Enviar"/>'
+        return '''
+        <input type="hidden" name="data" value=""/>
+        <input type="button" name="sendButton" value="Testar"/>
+        <input type="button" name="resetButton" value="Reset"/>
+        '''
 
 
--- src/active_handout_theme/assets/js/parsons.js
+++ src/active_handout_theme/assets/js/parsons.js
@@ -1,104 +1,116 @@
-function createElementWithAttributes(tag, attributes) {
-    const e = document.createElement(tag);
-    for (const [attr, val] of Object.entries(attributes)) {
-        e.setAttribute(attr, val);
+function convertRemToPixels(rem) {
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+function convertPixelsToRem(px) {
+    return px / parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+function selectCurrentLine(evt, answerDropArea) {
+    const elementsBellowMouse = document.elementsFromPoint(evt.clientX, evt.clientY);
+    for (let i = 0; i < elementsBellowMouse.length; i++) {
+        if (elementsBellowMouse[i].classList.contains("parsons-line")) {
+            return elementsBellowMouse[i];
+        }
     }
-    return e;
+    return answerDropArea;
 }
 
-function findIndentLevel(line, tabSize) {
-    let spaces = 0;
-    for (const c of line) {
-        if (c != ' ') break;
-        spaces++;
-    }
-    return Math.floor(spaces / tabSize);
-}
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("div.admonition.question.parsons").forEach((exercise) => {
+        let dragged = null;
 
-function splitCodeIntoLI(codeString) {
-    const items = [];
-    let count = 0;
-
-    for (const line of codeString.split("\n")) {
-        const noSpaces = line.trim();
-        if (noSpaces === "") continue;
-
-        const indentLevel = findIndentLevel(line, 4);
-        const newLI = createElementWithAttributes('li', {'draggable': true, "data-indent": indentLevel, "data-order": count});
-        newLI.innerText = line.substring(indentLevel * 4);
-        items.push(newLI);
-
-        count++;
-     }
-
-    return items;
-}
-
-let i = splitCodeIntoLI(`
-def py(as):
-    print(as)
-    return as + 3;
-`);
+        const answerDropArea = exercise.querySelector(".parsons-drop-area");
+        const originalArea = exercise.querySelector(".parsons-drag-area");
+        const indentLine = exercise.querySelector(".indent-line");
 
 
+        const allIndentClasses = Array.from(new Array(10), (x, i) => `indent-level-${i}`);
 
-const l = document.getElementById("p");
-for (const ll of i) {
-    l.appendChild(ll);
-}
-const answerBox = document.getElementById("answerBox");
+        answerDropArea.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            const dropAreaBB = answerDropArea.getBoundingClientRect();
+            indentLine.style.display = "block";
+            indentLine.style.top = (e.clientY - dropAreaBB.y) + "px";
+            const indentLevel = Math.trunc(convertPixelsToRem(e.clientX - dropAreaBB.left) / 2);
+            indentLine.classList.remove(...allIndentClasses);
+            indentLine.classList.add(`indent-level-${indentLevel}`);
 
-let dragged = null;
+            answerDropArea.querySelectorAll(".parsons-line").forEach((p) => p.classList.remove("line-drop-active"));
+            const dropEl = selectCurrentLine(e, answerDropArea);
+            if (dropEl === answerDropArea || dropEl.parentElement !== answerDropArea) {
+                // answerDropArea.appendChild(dragged);
+            } else {
+                dropEl.classList.add("line-drop-active");
+            }
+        });
 
-for (const c of l.children) {
-    c.draggable = true;
-    c.addEventListener("dragstart", (e) => {
-        dragged = e.target;
+        answerDropArea.addEventListener("drop", (e) => {
+            const draggedParent = dragged.closest(".admonition.parsons");
+            if (exercise === draggedParent) {
+                dragged.classList.remove(...allIndentClasses);
+                answerDropArea.querySelectorAll(".parsons-line").forEach((p) => p.classList.remove("line-drop-active"));
+                dragged.remove();
+
+                for (let cls of allIndentClasses) {
+                    if (indentLine.className.indexOf(cls) >= 0) {
+                        dragged.classList.add(cls);
+                        break;
+                    }
+                }
+
+                const dropEl = selectCurrentLine(e, answerDropArea);
+                if (dropEl === answerDropArea || dropEl.parentElement !== answerDropArea) {
+                    answerDropArea.appendChild(dragged);
+                } else {
+                    answerDropArea.insertBefore(dragged, dropEl);
+                }
+
+            }
+            dragged = null;
+            indentLine.style.display = "none";
+            e.preventDefault();
+        })
+
+        exercise.querySelectorAll(".parsons-line").forEach((line) => {
+            line.addEventListener("dragstart", (e) => {
+                dragged = e.target;
+            });
+        });
+
+        exercise.querySelector("input[name=resetButton]").addEventListener("click", () => {
+            answerDropArea.querySelectorAll(".parsons-line").forEach((line) => {
+                line.remove();
+                line.classList.remove(...allIndentClasses);
+                originalArea.appendChild(line);
+            })
+        });
+
+        exercise.querySelector("input[name=sendButton]").addEventListener("click", () => {
+            let correct = originalArea.querySelectorAll(".parsons-line").length == 0;
+            if (!correct) {
+                exercise.classList.remove("wrong");
+                void exercise.offsetLeft;
+                exercise.classList.add("wrong");
+                return;
+            }
+            answerDropArea.querySelectorAll(".parsons-line").forEach((line, idx) => {
+                const correctLineNum = parseInt(line.querySelector("a").id.split("-")[2]);
+                const correctIndent = parseInt(line.dataset.indentcount);
+                if (correctLineNum !== (idx + 1) ||
+                    !line.classList.contains(`indent-level-${correctIndent}`)) {
+                    correct = false;
+                }
+            });
+            if (correct) {
+                alert("Sua solução está correta!");
+            } else {
+                exercise.classList.remove("wrong");
+                void exercise.offsetLeft;
+                exercise.classList.add("wrong");
+            }
+
+        });
     });
-}
-
-
-
-answerBox.addEventListener("dragover", (event) => {
-  // prevent default to allow drop
-  event.preventDefault();
 });
 
-answerBox.addEventListener("drop", (e) => {
-    const dropEl = document.elementFromPoint(e.clientX, e.clientY);
-    
-    const answerBoxBound = answerBox.getBoundingRect();
-    
-    // TODO: indentLevel relativo ao answerBox!!
-    const elementX = e.pageX - answerBox.left;
-    const indentLevel = Math.round(e.clientX / 70);
-    console.log(elementX);
-    dragged.className = `indent${indentLevel}`;
-
-    if (dropEl === dragged) return true;
-
-    dragged.remove();
-    if (dropEl !== answerBox) {
-        answerBox.insertBefore(dragged, dropEl);
-    } else {
-        answerBox.append(dragged);
-    }
-
-    dragged = null;
-    return true;
-});
-
-
-document.getElementById("check").addEventListener("click", (e) => {
-    let counter = 0;
-    for (const c of answerBox.children) {
-        if (c.dataset["order"] == counter) console.log(`linha ${counter} no lugar certo!`);
-        else console.log("linha no lugar errado!");
-        
-        const expectedIndent = c.dataset["indent"];
-        if (c.classList.contains(`indent${expectedIndent}`)) console.log(`linha ${counter} indentação correta!`);
-        else console.log("indentação incorreta");
-             
-        counter++;
-    }
-});
