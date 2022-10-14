b6f85e808dc28b3da91da2414ca06e3decdd2108
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2022-10-14 12:34:35 -0300

    Fix vh max-height on mobile

--- src/active_handout_theme/assets/js/active-handout.a4a697aa.js
+++ src/active_handout_theme/assets/js/active-handout.a4a697aa.js
@@ -998,6 +998,12 @@ parcelHelpers.export(exports, "initStyle", ()=>initStyle);
 function initStyle() {
     const header = document.getElementsByClassName("ah-header")[0];
     document.documentElement.style.setProperty("--header-height", `${header.offsetHeight}px`);
+    // We listen to the resize event
+    window.addEventListener("resize", ()=>{
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
 }
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"5oERU"}]},["1csOT"], null, "parcelRequirea86e")
