304f230e8a68494c61d86df3d403b8d017d5ba1f
Igor Montagner <igordsm@gmail.com>
2022-11-05 11:49:15 -0500

    Add float random values and do statement to set random seed.

--- src/active_handout_plugins/templating.py
+++ src/active_handout_plugins/templating.py
@@ -16,6 +16,19 @@ class RandomIntVariable:
         return str(self.value)
 
 
+class RandomFloatVariable:
+    def __init__(self):
+        self.new()
+
+    def new(self, start=0, end=10):
+        sz = end - start
+        self.value = sz * random.random() + start
+        return str(self.value)
+
+    def __str__(self):
+        return str(self.value)
+
+
 class Chooser:
     def __init__(self):
         pass
@@ -27,12 +40,17 @@ class Chooser:
 class Jinja2PreProcessor(Preprocessor):
     def run(self, lines):
         text = '\n'.join(lines)
-        e = Environment()
+        e = Environment(extensions=['jinja2.ext.do'])
         random_elements = {
             'choose': Chooser(),
+            'seed': random.seed,
         }
         random_elements.update(
-            {f'randint{i}': RandomIntVariable() for i in range(1, 11)})
+            {f'randint{i}': RandomIntVariable() for i in range(1, 11)}
+        )
+        random_elements.update(
+            {f'randfloat{i}': RandomFloatVariable() for i in range(1, 11)}
+        )
         new_text = e.from_string(text).render(random_elements)
 
         return new_text.split('\n')
