b8fdcae65ea5d73f53efd50b10a10f736245aa80
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2022-12-28 15:50:06 -0300

    Add user-info endpoint and allow redirections to vscode (#48)
    * Add user-info endpoint and allow redirections to vscode
    
    * Add tests

--- backend/app/core/tests.py
+++ backend/app/core/tests.py
@@ -1,9 +1,12 @@
 from django.utils.translation import gettext as _
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from rest_framework.test import APIRequestFactory, force_authenticate
+from rest_framework.authtoken.models import Token
+from django.core.exceptions import DisallowedRedirect
 
 from core.models import Instructor, User, Exercise, Course, ExerciseTag
-from core.views import ensure_tags_equal, telemetry_data
+from core.views import ensure_tags_equal, telemetry_data, login_request
+from core.shortcuts import redirect
 
 
 class StudentAndInstructorTests(TestCase):
@@ -13,6 +16,30 @@ class StudentAndInstructorTests(TestCase):
             password='12'
         )
         assert self.prof1.is_staff == True
+
+
+class AuthTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='gandalf', email='thegray@middleearth.nz', password='you-shall-not-pass')
+        self.token = Token.objects.create(user=self.user)
+
+    def test_redirects_to_vscode_if_logged_in(self):
+        next = 'vscode://domain.extension'
+        request = self.factory.get(f'/api/login?next={next}')
+        request.user = self.user
+        request.session = {}
+
+        response = login_request(request)
+        assert response.url == f'{next}?token={self.token}'
+
+    def test_doesnt_redirect_to_unauthorized_scheme(self):
+        next = 'wrong://domain.extension'
+        request = self.factory.get(f'/api/login?next={next}')
+        request.user = self.user
+        request.session = {}
+
+        self.assertRaises(DisallowedRedirect, login_request, request)
 
 
 class TelemetryDataTests(TestCase):
@@ -66,3 +93,12 @@ class TelemetryDataTests(TestCase):
         assert response.status_code == 200, f'Wrong status code. Expected 200, got {response.status_code}'
         assert Course.objects.filter(name=data['exercise']['course']).exists()
         assert Exercise.objects.filter(slug=data['exercise']['slug']).exists()
+
+
+class RedirectTests(TestCase):
+    def test_redirect_allows_vscode_scheme(self):
+        response = redirect('vscode://domain.extension')
+        assert response is not None
+
+    def test_redirect_disallows_other_schemes(self):
+        self.assertRaises(DisallowedRedirect, redirect, 'wrong://domain.extension')
