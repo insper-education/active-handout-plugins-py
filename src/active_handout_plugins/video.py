f3e4b50004df8c00e598b1c9a175905b2221732e
Rafael Corsi <corsiferrao@gmail.com>
2023-01-15 09:02:20 -0300

    fix regex

--- src/active_handout_plugins/video.py
+++ src/active_handout_plugins/video.py
@@ -7,9 +7,10 @@ from .admonition import AdmonitionVisitor
 class VideoAdmonition(AdmonitionVisitor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.re = re.compile(r'https?:\/\/(www\.)?youtube\.com\/watch?.*v=([a-zA-Z0-9]+)&?.*')
+        self.re = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
 
     def visit(self, el):
+
         if not 'video' in el.attrib['class']:
             return
 
@@ -17,8 +18,9 @@ class VideoAdmonition(AdmonitionVisitor):
         if img_el is not None:
             src = img_el.attrib['src']
             m = self.re.match(src)
+
             if m:
-                html = f'<iframe width="100%" height="500" type="text/html" src="https://www.youtube.com/embed/{m.group(2)}?autoplay=0"></iframe>'
+                html = f'<iframe width="100%" height="500" type="text/html" src="https://www.youtube.com/embed/{m.group("id")}?autoplay=0"></iframe>'
             else:
                 html = f'<video width="100%" src="{src}" controls="true" />'
 
