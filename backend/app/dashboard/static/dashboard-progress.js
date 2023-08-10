258e018d0c7aa4b327b4f8da4ca93c7f274f5172
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2023-08-10 15:46:25 -0500

    Add course class model and filter students by class in progress dashboard (+ some CSS stuff)

--- backend/app/dashboard/static/dashboard-progress.js
+++ backend/app/dashboard/static/dashboard-progress.js
@@ -1,151 +1,161 @@
-
 function createHandsontable(data, columns_list) {
+  data = filterStudentsInClass(data);
 
-  var container = document.getElementById('table');
-  Handsontable.renderers.registerRenderer('colorFormattingRenderer', function (
-    instance,
-    td,
-    row,
-    col,
-    prop,
-    value,
-    cellProperties
-  ) {
-    cellProperties.editor = false;
-    Handsontable.renderers.TextRenderer.apply(this, arguments);
-    td.style.color = 'black';
-    var cell_value = parseFloat(value);
-    if (cell_value == 1) {
-      td.style.background = 'green';
+  const container = document.getElementById("table");
+  Handsontable.renderers.registerRenderer(
+    "colorFormattingRenderer",
+    function (instance, td, row, col, prop, value, cellProperties) {
+      cellProperties.editor = false;
+      Handsontable.renderers.TextRenderer.apply(this, arguments);
+      td.style.color = "black";
+      const cell_value = parseFloat(value);
+      if (cell_value == 1) {
+        td.style.background = "green";
+      } else if (cell_value == 0) {
+        td.style.background = "red";
+      } else if (cell_value > 0 && cell_value < 1) {
+        td.style.background = "#ffae00";
+      }
     }
-    else if (cell_value == 0) {
-      td.style.background = 'red';
-    }
-    else if ((cell_value > 0) && (cell_value < 1)) {
-      td.style.background = '#ffae00'
-    }
-  });
+  );
 
   hot = new Handsontable(container, {
     data: data,
     colHeaders: columns_list,
     columns: function (column) {
       columnMeta = {};
-      columnMeta.data = columns_list[column]
+      columnMeta.data = columns_list[column];
       return columnMeta;
     },
     colWidths: [100].concat(Array(columns_list.length - 1).fill(30)),
     fixedColumnsStart: 1,
     cells: function (row, col, prop) {
-      var cellProperties = {};
-      cellProperties.renderer = 'colorFormattingRenderer';
+      const cellProperties = {};
+      cellProperties.renderer = "colorFormattingRenderer";
       return cellProperties;
     },
-    licenseKey: 'non-commercial-and-evaluation', // for non-commercial use only
-
+    licenseKey: "non-commercial-and-evaluation", // for non-commercial use only
   });
 }
 
 function updateHandsontable(data, columns_list) {
+  data = filterStudentsInClass(data);
 
   hot.updateSettings({
     data: data,
     colHeaders: columns_list,
     columns: function (column) {
       columnMeta = {};
-      columnMeta.data = columns_list[column]
+      columnMeta.data = columns_list[column];
       return columnMeta;
-    }, colWidths: [100].concat(Array(columns_list.length - 1).fill(30)),
+    },
+    colWidths: [100].concat(Array(columns_list.length - 1).fill(30)),
   });
 }
 
+function getTagSelect() {
+  return document.getElementById("select-tag");
+}
+
+function getCurrentStudents() {
+  const classSelect = document.getElementById("select-class");
+  const selectedClass = courseClasses[classSelect.selectedIndex];
+  return selectedClass.students;
+}
+
+function filterStudentsInClass(data) {
+  const currentStudents = getCurrentStudents();
+  return data.filter((row) => currentStudents.has(row.Name));
+}
+
 function updateFilter() {
-
-  var select = document.getElementById("select-tag");
-  var newValue = select.value;
-
+  const select = getTagSelect();
+  const newValue = select.value;
   select.value = "";
 
-  if (!Object.keys(tagsObj).includes(newValue))
-    return;
+  if (!Object.keys(tagsObj).includes(newValue)) return;
   tags.add(newValue);
   generateTagView();
   updateTableContent();
 }
 
 function updateTableContent() {
+  const clonedData = structuredClone(data);
+  const clonedColumns = structuredClone(columns);
 
-  var clonedData = structuredClone(data);
-  var clonedColumns = structuredClone(columns);
-
-  var filteredColumns = ["Name"]
-  exerciseList = []
+  let filteredColumns = ["Name"];
+  exerciseList = [];
   if (tags.size == 0) {
     updateHandsontable(clonedData, clonedColumns);
     return;
   }
-  tags.forEach(tag => {
+  tags.forEach((tag) => {
     exerciseList = exerciseList.concat(tagsObj[tag]);
   });
   for (var i = 0; i < exerciseList.length; i++) {
-    var column_index = clonedColumns.indexOf(exerciseList[i]);
+    const column_index = clonedColumns.indexOf(exerciseList[i]);
     if (column_index != -1)
-      filteredColumns = filteredColumns.concat(clonedColumns.splice(column_index,1));
+      filteredColumns = filteredColumns.concat(
+        clonedColumns.splice(column_index, 1)
+      );
   }
 
   updateHandsontable(clonedData, filteredColumns);
 }
+
 function removeTag(ev) {
   tags.delete(ev.target.id);
   generateTagView();
-  updateTableContent()
+  updateTableContent();
 }
 
 function generateTagView() {
-  var tagsDiv = document.getElementById("tags-list");
+  const tagsDiv = document.getElementById("tags-list");
   tagsDiv.innerHTML = "";
-  tags.forEach(element => {
-    var tagDiv = document.createElement("div");
+  tags.forEach((element) => {
+    const tagDiv = document.createElement("div");
     tagDiv.className = "tag";
-    var remove = document.createElement("button")
-    remove.className = "btn btn-secondary"
+    const remove = document.createElement("button");
+    remove.className = "btn btn-secondary";
     remove.onclick = removeTag;
-    remove.innerText = element
+    remove.innerText = element;
     remove.id = element;
     tagDiv.appendChild(remove);
     tagsDiv.appendChild(tagDiv);
-
   });
 }
-
 
 var tags = new Set();
 var table;
 var columns;
 var data;
+var courseClasses;
 var tags;
 var hot;
 
-document.addEventListener('DOMContentLoaded', function () {
-
-  table = document.getElementById('table');
-  columns = table.getAttribute('data-columns');
-  data = table.getAttribute('data-data');
-  tagsObj = table.getAttribute('data-tags')
+document.addEventListener("DOMContentLoaded", function () {
+  table = document.getElementById("table");
+  columns = table.getAttribute("data-columns");
+  data = table.getAttribute("data-data");
+  tagsObj = table.getAttribute("data-tags");
+  courseClasses = table.getAttribute("data-classes");
 
   //cleaning
   data = data.replace(/'/g, '"');
   columns = columns.replace(/'/g, '"');
+  courseClasses = courseClasses.replace(/'/g, '"');
   tagsObj = tagsObj.replace(/'/g, '"').replace(/(\w+):/g, '"$1":');
 
-
-
   data = JSON.parse(data);
-  columns = JSON.parse(columns)
+  columns = JSON.parse(columns);
+  courseClasses = JSON.parse(courseClasses);
   tagsObj = JSON.parse(tagsObj);
 
+  courseClasses.forEach((courseClass) => {
+    courseClass.students = new Set(courseClass.students);
+  });
 
-  var select = document.getElementById("select-tag")
+  const select = getTagSelect();
   select.onchange = updateFilter;
 
   tableData = structuredClone(data);
