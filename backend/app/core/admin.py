601974f39da80627397886fde871cdcfb36a06dd
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2023-08-15 13:59:49 -0500

    Add course class model and filter students by class in progress dashboard (#87)
    * Add course class model and filter students by class in progress dashboard (+ some CSS stuff)
    
    * Update table when user changes class
    
    * Fix dataSchema only showing the columns of the first student
    
    ---------
    
    Co-authored-by: zMendes <leonardom1808@gmail.com>

--- backend/app/core/admin.py
+++ backend/app/core/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Student, Instructor, Course, Exercise, ExerciseTag, TelemetryData
+from .models import Student, Instructor, Course, CourseClass, Exercise, ExerciseTag, TelemetryData
 
 
 admin.site.register(Student, UserAdmin)
 admin.site.register(Instructor, UserAdmin)
 admin.site.register(Course)
+admin.site.register(CourseClass)
 admin.site.register(Exercise)
 admin.site.register(ExerciseTag)
 admin.site.register(TelemetryData)
