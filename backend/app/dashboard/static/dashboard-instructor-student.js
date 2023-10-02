afd039c2acde05f32e2071861ffdeed7be1f98c5
zMendes <leonardom1808@gmail.com>
2023-10-02 16:18:31 -0300

    Formatted code block

--- backend/app/dashboard/static/dashboard-instructor-student.js
+++ backend/app/dashboard/static/dashboard-instructor-student.js
@@ -28,11 +28,11 @@ function createTagChart(data) {
         options: {
             onClick: function handleBarClick(event, activeElements) {
                 if (activeElements.length > 0) {
-                  let index = activeElements[0].index;
-                  createTable(data[labels[index]].data);
-                  //createAnswerView(data[labels[index]].data);
+                    let index = activeElements[0].index;
+                    clearCodeBlock();
+                    createTable(data[labels[index]].data);
                 }
-              },
+            },
             indexAxis: "y",
             responsive: false,
             maintainAspectRatio: true,
@@ -48,33 +48,42 @@ function createTagChart(data) {
     });
 }
 
-function createAnswerView(slug, data){
-    let div = document.getElementById("answers");
-    for (let i=0; i<Object.keys(data).length; i++){
-        div.appendChild(createAccordionItem(slug, data[i], i));
-        }
-}
-
-function createTable(data){
+function createTable(data) {
+    tableData = data;
     let keys = Object.keys(data);
     let formatedData = []
-    for (let i=0; i<keys.length; i++){
+    for (let i = 0; i < keys.length; i++) {
         formatedData.push([keys[i], data[keys[i]].length]);
     }
-    let table = document.getElementById("table");
-    const hot = new Handsontable(table, {
+    if (table) {
+        table.updateSettings({ data: formatedData });
+        return;
+    }
+    let tableDiv = document.getElementById("table");
+    table = new Handsontable(tableDiv, {
         data: formatedData,
         colHeaders: ["Slug", "Submissions"],
         columns: [
-            {data: 0},
-            {data: 1},
+            { data: 0 },
+            { data: 1 },
         ],
         licenseKey: "non-commercial-and-evaluation"
-      });
-      hot.addHook('afterSelectionByProp', (row, prop) => {
-        let slug = hot.getSourceDataAtRow(row)[0];
-        createAnswerView(slug, data[slug]);
-      });
+    });
+    table.addHook('afterSelectionByProp', onRowClicked);
+}
+
+function onRowClicked(row, prop) {
+    let slug = table.getSourceDataAtRow(row)[0];
+    clearCodeBlock();
+    createAnswerView(slug, tableData[slug]);
+}
+
+function createAnswerView(slug, data) {
+    let div = document.getElementById("answers");
+    div.innerHTML = "";
+    for (let i = 0; i < Object.keys(data).length; i++) {
+        div.appendChild(createAccordionItem(slug, data[i], i));
+    }
 }
 
 function createAccordionItem(slug, data, index) {
@@ -91,7 +100,7 @@ function createAccordionItem(slug, data, index) {
     button.setAttribute("data-bs-target", `#collapse-${index}`);
     button.setAttribute("aria-expanded", "false"); // Change this to "true" for the first item
     button.setAttribute("aria-controls", `collapse-${index}`);
-    button.textContent = slug + " - Submissão #" + (index+1);
+    button.textContent = slug + " - Submissão #" + (index + 1);
 
     const collapse = document.createElement("div");
     collapse.classList.add("accordion-collapse", "collapse");
@@ -99,12 +108,7 @@ function createAccordionItem(slug, data, index) {
 
     const body = document.createElement("div");
     body.classList.add("accordion-body");
-    if (data.log.code)
-        body.innerHTML = data.log.code ;
-    else if (data.log.student_input)
-        body.innerHTML = data.log.student_input["solution.py"];
-    else
-        body.innerHTML = data.log;
+    body.appendChild(createCodeBlock(data.log))
 
     collapse.appendChild(body);
     header.appendChild(button);
@@ -112,12 +116,54 @@ function createAccordionItem(slug, data, index) {
     item.appendChild(collapse);
 
     return item;
-  }
+}
 
+function createCodeBlock(data) {
+
+    if (data.code) {
+        return createCode(data.code, "");
+
+    }
+    else if (data.student_input) {
+        let codeContainer = document.createElement("div");
+
+        Object.keys(data.student_input).forEach(key => {
+            codeContainer.appendChild(createCode(data.student_input[key], key),);
+        });
+        return codeContainer;
+
+    }
+
+
+}
+function createCode(data, fileName) {
+    let codeDiv = document.createElement("div");
+    let codeSnippet = document.createElement("div");
+    codeSnippet.className = "code-snippet";
+    let fileNameDiv = document.createElement("div");
+    fileNameDiv.className = "file-name";
+    fileNameDiv.innerHTML = fileName;
+
+    let pre = document.createElement("pre");
+    let code = document.createElement("code");
+    code.className = "language-python";
+    code.innerHTML = data;
+    pre.appendChild(code);
+    codeSnippet.appendChild(pre);
+    Prism.highlightElement(code);
+    codeDiv.appendChild(fileNameDiv);
+    codeDiv.appendChild(codeSnippet);
+    return codeDiv;
+
+}
+function clearCodeBlock() {
+    document.getElementById("answers").innerHTML = "";
+}
 var activeCourse;
 var selectStudent;
-
 var tagChart;
+var table;
+var tableData;
 document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("data").style.visibility = "hidden";
